0af83cec4cd936113f99f27d0b2923a1f8fdd597
mark_lu <marklureal@gmail.com>
2018-08-27 23:48:02 +0800

    Add data persistent function

--- todoey/TodoListTableViewController.swift
+++ todoey/TodoListTableViewController.swift
@@ -11,9 +11,16 @@ import UIKit
 class TodoListViewController: UITableViewController {
     var itemArray = ["Find Mike", "Buy Eggos", "Destory Demogorgon"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let items = defaults.array(forKey: "TodoListArry") as? [String]
+        {
+            itemArray = items
+        }
+        
     }
 
     //MARK - TableView Data
@@ -53,6 +60,9 @@ class TodoListViewController: UITableViewController {
         
         let alertAction = UIAlertAction(title: "Add Item", style: .default) { (action) in
            self.itemArray.append(textField.text!)
+        
+           self.defaults.setValue(self.itemArray, forKey: "TodoListArry")
+           
            self.tableView.reloadData()
         }
         
